fff36c99a87984e2d1b59b420c91b481503d67d0
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 17:05:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  if counter > 10
+  while counter > 0 do
+    puts "#{counter} SECONDS(S)!"
+    counter -= 1
+  end
+end
+
+def countdown_with_sleep
+
 end
